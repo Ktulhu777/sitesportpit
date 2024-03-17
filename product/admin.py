9a5036863c0032946a9c6a6d1064d43b2769b5fc
Ktulhu777 <asusdag40@gmail.com>
2024-03-17 15:40:22 +0300

    add-last

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from product.models import Product, CategoryProduct
+
+admin.site.register(Product)
+
+
+@admin.register(CategoryProduct)
+class CategoryProductAdmin(admin.ModelAdmin):
+    fields = ('cat_name', 'slug',)
+    prepopulated_fields = {"slug": ("cat_name",)}
