ddb3279076dadc83a75c31ad1057b4ca9dac895d
Ktulhu777 <asusdga40@gmail.com>
2024-03-17 14:53:12 +0300

    add

--- product/urls.py
+++ product/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('api/v1/product-list/', views.ProductViewSet.as_view({'get': 'list'})),
     path('api/v1/product-list/<slug:slug>/', views.ProductViewSet.as_view({'get': 'list'})),
+    # что то
+
 ]
