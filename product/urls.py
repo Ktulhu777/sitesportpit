f834035426eb95e5c289317249a350d896d8b05b
Ktulhu777 <asusdga40@gmail.com>
2024-03-17 15:03:33 +0300

    first

--- product/urls.py
+++ product/urls.py
@@ -5,6 +5,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('api/v1/product-list/', views.ProductViewSet.as_view({'get': 'list'})),
     path('api/v1/product-list/<slug:slug>/', views.ProductViewSet.as_view({'get': 'list'})),
-    # что то
-
 ]
