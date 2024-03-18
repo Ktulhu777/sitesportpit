73a028b847c0b0d9d09edae3c6d827559f39f375
VKalaitanov <kalaytanov93@gmail.com>
2024-03-18 13:53:07 +0100

    add api register users

--- users/views.py
+++ users/views.py
@@ -4,7 +4,8 @@ from .serializers import UserSerializer
 from .permissions import UserPermission
 from .models import User
 
+
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all().order_by('-date_joined')
-    permission_classes = (UserPermission, )
+    permission_classes = (UserPermission,)
