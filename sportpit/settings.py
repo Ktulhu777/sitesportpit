98dc7d516f0b30d98c50388649d67539d4028f34
Ktulhu777 <asusdag40@gmail.com>
2024-03-19 16:12:56 +0300

    'database'

--- sportpit/settings.py
+++ sportpit/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'sportpit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sitesportpit_db',
+        'USER': 'sitesportpit',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'POST': 8080,
     }
 }
 
