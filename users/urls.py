8d8f7e877eeeee285852cab1c860726fcb4c4843
Ktulhu777 <asusdag40@gmail.com>
2024-03-22 13:20:48 +0300

    'registation-bug'

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path, include
-from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
-from .views import UserViewSet
+from . import views
 
-
-router = routers.SimpleRouter()
-router.register('users', UserViewSet)
-# app_name = "users"
-
-
-urlpatterns = [  # ↓ http://127.0.0.1:8000/user/api/users/ ↓
-    path('api/', include(router.urls)),  # => registration user, form: username, password
+urlpatterns = [  # ↓ http://127.0.0.1:8000/user/registration/ ↓
+    path('registration/', views.UserViewSet.as_view({'post': 'create'})),
+    # => registration user, form: username, password
     path('api-auth/', include('rest_framework.urls')),  # => in end_url /login/ or  /logout/
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # give jwt-token - access and refresh
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # give access token by refresh
