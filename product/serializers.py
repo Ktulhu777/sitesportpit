3ce83a7d1f304e60ebeb0a0d7b3692f801c5ee38
VKalaitanov <kalaytanov93@gmail.com>
2024-03-27 11:31:44 +0100

    change cat fields

--- product/serializers.py
+++ product/serializers.py
@@ -3,12 +3,14 @@ from .models import Product, CategoryProduct
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    cat = serializers.CharField()
+
     class Meta:
         model = Product
-        fields = ('id', 'name', 'description', 'img', 'price', 'discount', 'time_create', 'cat_name')
+        fields = ('id', 'name', 'description', 'img', 'price', 'discount', 'time_create', 'cat')
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = CategoryProduct
-        fields = ('cat_name', )
+        fields = ('cat_name',)
