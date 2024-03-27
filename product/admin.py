b06a9548be4b82255ba5b0e6667bf3b3dac9f2c2
VKalaitanov <kalaytanov93@gmail.com>
2024-03-27 10:47:43 +0100

    change fields in admin.py

--- product/admin.py
+++ product/admin.py
@@ -5,18 +5,18 @@ from .models import Product, CategoryProduct
 
 @admin.register(Product)
 class ProductModel(admin.ModelAdmin):
-    fields = ('title', 'content', 'photo', 'post_photo', 'is_published', 'price', 'cat')
-    ordering = ('-time_create', 'title',)
-    readonly_fields = ('post_photo',)
-    list_display = ('title', 'post_photo', 'time_create', 'is_published', 'cat')
-    list_display_links = ('title',)
+    fields = ('name', 'description', 'img', 'product_img', 'is_published', 'price', 'cat')
+    ordering = ('-time_create', 'name',)
+    readonly_fields = ('product_img',)
+    list_display = ('name', 'product_img', 'time_create', 'is_published', 'cat')
+    list_display_links = ('name',)
     save_on_top = True
 
-    @admin.display(description="Изображение", ordering='content')
-    def post_photo(self, product: Product):
+    @admin.display(description="Изображение", ordering='description')
+    def product_img(self, product: Product):
         """Функция вывода изображения, если оно есть"""
-        if product.photo:
-            return mark_safe(f"<img src='{product.photo.url}' width=50>")
+        if product.img:
+            return mark_safe(f"<img src='{product.img.url}' width=50>")
         return "Без фото"
 
 
