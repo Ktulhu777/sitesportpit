9619e2887c995530940c9888cd7dc04f379bb45b
VKalaitanov <kalaytanov93@gmail.com>
2024-03-29 15:43:20 +0100

    change date_birth field

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class User(AbstractUser):
     date_joined = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания учетной записи')
     phone = models.CharField(blank=True, max_length=14, verbose_name='Номер телефона')
-    date_birth = models.DateTimeField(blank=True, null=True, verbose_name="Дата рождения")
+    date_birth = models.DateField(blank=True, null=True, verbose_name="Дата рождения")
     city = models.CharField(blank=True, max_length=50)
 
     def __str__(self):
