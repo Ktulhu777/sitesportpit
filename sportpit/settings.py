97cd145bba44cc9ff24e32264cbb1b04d7295654
Ktulhu777 <asusdag40@gmail.com>
2024-04-02 12:36:39 +0300

    'last'

--- sportpit/settings.py
+++ sportpit/settings.py
@@ -26,7 +26,8 @@ CSRF_TRUSTED_ORIGINS = env('CSRF_TRUSTED_ORIGINS').split()
 
 # ALLOWED_HOSTS = ['127.0.0.1']
 # INTERNAL_IPS = ["127.0.0.1"]
-CORS_ALLOWED_ORIGINS = ['http://localhost:8000', 'http://localhost:3000']
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = ['http://localhost:8000', 'http://localhost:3000']
 # CORS_ALLOW_CREDENTIALS = True
 
 CACHES = {
