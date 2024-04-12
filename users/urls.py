244ca92e3a24a7e0b650b8036f3258ce26f6cfc9
VKalaitanov <kalaytanov93@gmail.com>
2024-04-12 13:13:13 +0200

    add url api

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("", include("djoser.urls.jwt")),
     path('account/activate/<uid>/<token>', views.ActivateUser.as_view({'get': 'activation'}), name='activation'),
     path('api-auth/', include('rest_framework.urls')),  # => in end_url /login/ or  /logout/
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # give jwt-token - access and refresh
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # give access token by refresh
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),  # unknown
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # give jwt-token - access and refresh
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # give access token by refresh
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),  # unknown
 ]
