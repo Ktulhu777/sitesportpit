4179afa8af9747376780f7690cb1de459fe3d820
VKalaitanov <kalaytanov93@gmail.com>
2024-04-15 13:21:27 +0200

    add slug field

--- product/admin.py
+++ product/admin.py
@@ -10,7 +10,7 @@ class GalleryInline(admin.TabularInline):
 
 @admin.register(Product)
 class ProductModel(admin.ModelAdmin):
-    fields = ('name', 'description', 'is_published', 'price', 'discount_price', 'category', 'quantity')
+    fields = ('name', 'slug', 'description', 'is_published', 'price', 'discount_price', 'category', 'quantity')
     ordering = ('-time_create', 'name',)
     # readonly_fields = ('product_img',)
     list_display = ('name', 'time_create', 'is_published', 'category')
