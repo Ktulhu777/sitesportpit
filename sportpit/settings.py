cbcceab331ad6c723f810dec0ce1606401a85fdd
Ktulhu777 <asusdag40@gmail.com>
2024-04-16 21:36:40 +0300

    'fix-bug-filters'

--- sportpit/settings.py
+++ sportpit/settings.py
@@ -176,6 +176,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
 
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
+
     # 'DEFAULT_PARSER_CLASSES': [
     #     'rest_framework.parsers.JSONParser',
     # ],
