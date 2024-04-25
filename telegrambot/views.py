5c09b08c94879716cc7f874173fe6ca01cdc79e1
VKalaitanov <kalaytanov93@gmail.com>
2024-04-25 13:23:04 +0200

    add telegramviews

--- telegrambot/views.py
+++ telegrambot/views.py
@@ -1,24 +1,17 @@
-from django.contrib.auth import get_user_model
 from rest_framework.views import APIView
-from .serializers import TelegramSerializer
 from rest_framework.response import Response
 
 
 class TelegramViews(APIView):
-    def put(self):
+
+    def get(self, request, telegram_id):
         telegram_id = self.kwargs.get('telegram_id')
-        user = get_user_model().objects.get(pk=1)
-        user.telegram_id = telegram_id
-        user.save()
-        return Response({'detail': 'Данные успешно сохранены'})
+
+        user = self.request.user
+        if not user.telegram_id:
+            user.telegram_id = telegram_id
+            user.save()
+            return Response({'detail': 'Данные успешно сохранены'})
+        return Response({'detail': 'Ваш телеграм уже привязан'})
 
 
-def func(request, telegram):
-    telegram_id = telegram
-    print(telegram_id)
-    user = get_user_model().objects.get(username='user')
-    print(user)
-    user.telegram_id = telegram_id
-
-    user.save()
-    return Response({'detail': 'Данные успешно сохранены'})
