8c635c5cf1d0c502ef1c8b9f67e18fa33448b0a0
VKalaitanov <kalaytanov93@gmail.com>
2024-04-25 14:26:23 +0200

    add settings telegam bot

--- telegrambot/statebot.py
+++ telegrambot/statebot.py
@@ -1,6 +1,6 @@
-# from aiogram.fsm.state import StatesGroup, State
-#
-#
-# class Form(StatesGroup):
-#     email = State()
+from aiogram.fsm.state import StatesGroup, State
+
+
+class Form(StatesGroup):
+    email = State()
 
